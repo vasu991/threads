8742d37a9790b6aae4f61ebb044e76e1dfbaa4f4
Vasu Bhatia <vasubhatia991@gmail.com>
2023-08-19 21:05:21 +0530

    left&bottombar layout

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,42 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
+
 export default function Bottombar () {
+    const pathname = usePathname();
+    const router = useRouter();
     return (
-        <h1>Bottombar</h1>
+        <section className="bottombar">
+            <div className="bottombar_container">
+            {sidebarLinks.map((link) => {
+                    const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+                        return (
+
+                        <Link
+                            href={link.route}
+                            key={link.label}
+                            className={`bottombar_link ${isActive && 'bg-primary-500'}`} 
+                        >
+                            <Image
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={24}
+                                height={24}
+                            />
+
+                            <p className="text-subtle-medium text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}
+                            </p>
+
+                        </Link>
+                        )}
+                )}  
+
+            </div>
+        </section>
+       
     )
 }
